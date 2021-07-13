576f125d23b65dc9f927207f4a3aa8ae2eb2e3f1
pawelplutauek20 <pawelpluta58@gmail.com>
2021-07-14 00:33:05 +0200

    styled post details page

--- src/components/PostCard/PostCard.js
+++ src/components/PostCard/PostCard.js
@@ -1,7 +1,48 @@
+import { Paper, makeStyles, Typography, Grid, IconButton } from "@material-ui/core";
 import React from "react";
+import { Link, useParams } from "react-router-dom";
+import DeleteIcon from '@material-ui/icons/Delete';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(2),
+    minWidth: 300
+  },
+  post: {
+    display: "-webkit-box",
+    boxOrient: "vertical",
+    lineClamp: 1,
+    wordBreak: "break-all",
+    overflow: "hidden",
+  },
+  container: {
+    flexWrap: "nowrap"
+  },
+  button: {
+    marginLeft: "auto"
+  }
+}));
 
 const PostCard = ({ post }) => {
-  return <p>{post}</p>;
+  const classes = useStyles();
+  const { userId } = useParams();
+
+  return (
+    <Paper className={classes.paper}>
+      <Grid container className={classes.container} spacing={1} alignItems="center">
+        <Grid item>
+        <IconButton color="primary" fontSize="large" aria-label="delete"><DeleteIcon /></IconButton>
+        </Grid>
+        <Grid className={classes.post} item>
+          <Typography>{post.title}</Typography>
+        </Grid>
+        <Grid item className={classes.button}>
+          <Link to={`/user/${userId}/${post.id}`}><IconButton color="primary" fontSize="large" aria-label="next"><ArrowForwardIosIcon /></IconButton></Link>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
 };
 
 export default PostCard;
--- src/components/UserDetailsPage.js
+++ src/components/UserDetailsPage.js
@@ -1,8 +1,20 @@
+import { Container, Grid, makeStyles, Typography } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { Link as RouterLink } from "react-router-dom";
 import PostCard from "./PostCard/PostCard";
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import IconButton from '@material-ui/core/IconButton';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    marginTop: theme.spacing(6),
+  },
+}));
 
 const UserDetailPage = () => {
+  const classes = useStyles();
   const [user, setUser] = useState("");
   const [posts, setPosts] = useState([]);
 
@@ -23,16 +35,25 @@ const UserDetailPage = () => {
   }, [userId]);
 
   return (
-    <div>
-      <div>
-        <b>{user}</b>
-      </div>
-      <div>
-        {posts.map((post) => (
-          <PostCard key={post.id} post={post.title} />
-        ))}
-      </div>
-    </div>
+    <Container className={classes.container}>
+      <Grid container spacing={2}>
+        <Grid item container justifyContent="space-between" alignItems="center">
+          <RouterLink to="/"><IconButton color="primary" fontSize="large" aria-label="back"><ArrowBackIcon /></IconButton></RouterLink>
+          <Typography variant="h6">
+            <b>{user}</b>
+          </Typography>
+          <IconButton color="primary" fontSize="large" aria-label="add"><AddCircleIcon /></IconButton>
+        </Grid>
+
+        <Grid className={classes.container} item container spacing={2} direction="column">
+          {posts.map((post) => (
+            <Grid item xs={12} key={post.id}>
+              <PostCard post={post} />
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
