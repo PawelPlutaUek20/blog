41ede91dd61f12a99c0c38f7d71686185c6d2341
pawelplutauek20 <pawelpluta58@gmail.com>
2021-07-14 00:55:20 +0200

    styled post details page

--- src/components/CommentCard/CommentCard.js
+++ src/components/CommentCard/CommentCard.js
@@ -1,15 +1,32 @@
+import { Link, Typography } from "@material-ui/core";
+import { Grid, makeStyles, Paper } from "@material-ui/core";
 import React from "react";
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(2),
+  },
+}));
+
 const CommentCard = ({ comment }) => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <div>
-        <b>{comment.name}</b>
-      </div>
-      <a href="#/">{comment.email}</a>
-      <div>{comment.body}</div>
-      <br />
-    </div>
+    <Paper className={classes.paper}>
+      <Grid container spacing={2}>
+        <Grid item container justifyContent="space-between">
+          <Typography>
+            <b>{comment.name}</b>
+          </Typography>
+          <Link href="#/" underline="always">
+            <Typography>{comment.email}</Typography>
+          </Link>
+        </Grid>
+        <Grid item>
+          <Typography>{comment.body}</Typography>
+        </Grid>
+      </Grid>
+    </Paper>
   );
 };
 
--- src/components/PostDetailsPage.js
+++ src/components/PostDetailsPage.js
@@ -1,11 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import CommentCard from "./CommentCard/CommentCard";
+import {
+  makeStyles,
+  Container,
+  Grid,
+  Typography,
+  Link as MaterialLink,
+  IconButton,
+} from "@material-ui/core";
+import { Link as RouterLink } from "react-router-dom";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    marginTop: theme.spacing(6),
+  },
+  button: {
+    visibility: "hidden",
+    width: 48,
+  },
+}));
 
 const PostDetailPage = () => {
+  const classes = useStyles();
   const [user, setUser] = useState("");
   const [post, setPost] = useState({});
   const [comments, setComments] = useState([]);
+  const [show, setShow] = useState(false);
 
   const { userId, postId } = useParams();
 
@@ -28,19 +50,57 @@ const PostDetailPage = () => {
   }, [userId, postId]);
 
   return (
-    <div>
-      <div>
-        <b>{user}</b>
-      </div>
-      <h4>{post.title}</h4>
-      <div>{post.body}</div>
-      <br />
-      <div>
-        {comments.map((comment) => (
-          <CommentCard key={comment.id} comment={comment} />
-        ))}
-      </div>
-    </div>
+    <Container className={classes.container}>
+      <Grid container spacing={3}>
+        <Grid item container justifyContent="space-between" alignItems="center">
+          <RouterLink to={`/user/${userId}`}>
+            <IconButton color="primary" fontSize="large" aria-label="back">
+              <ArrowBackIcon />
+            </IconButton>
+          </RouterLink>
+          <Typography variant="h6">
+            <b>{user}</b>
+          </Typography>
+          <button className={classes.button}></button>
+        </Grid>
+        <Grid item className={classes.container}>
+          <Typography gutterBottom variant="h5">
+            <b>{post.title}</b>
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Typography gutterBottom>{post.body}</Typography>
+        </Grid>
+        <Grid item container justifyContent="space-between">
+          <MaterialLink
+            onClick={() => setShow(!show)}
+            href="#/"
+            underline="always"
+          >
+            {show ? (
+              <Typography>Hide commments</Typography>
+            ) : (
+              <Typography>Show comments</Typography>
+            )}
+          </MaterialLink>
+
+          {show ? (
+            <MaterialLink href="#/" underline="always">
+              <Typography>Add comment</Typography>
+            </MaterialLink>
+          ) : null}
+        </Grid>
+        {show ? (
+          <Grid item container spacing={2} direction="column">
+            {comments.map((comment) => (
+              <Grid item xs={12} key={comment.id}>
+                <CommentCard comment={comment} />
+              </Grid>
+            ))}
+          </Grid>
+        ) : null}
+      </Grid>
+    </Container>
   );
 };
 
