ae2ff04563e6dcf3626f67c4c6089bca345c6fad
pawelplutauek20 <pawelpluta58@gmail.com>
2021-07-13 12:02:45 +0200

    basic user card layout

--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -1,7 +1,27 @@
 import React from "react";
 
 const UserCard = ({ user }) => {
-  return <pre key={user.id}>{JSON.stringify(user, undefined, 2)}</pre>;
+  return (
+    <div>
+      <p>
+        <b>{user.name}</b>
+      </p>
+      <a href="/#">{user.email}</a>
+      <br />
+      <a href="/#">{user.phone.split(" ")[0]}</a>
+      <br />
+      <a href="/#">{user.website}</a>
+      <br />
+      <br />
+      <div>{user.company.name}</div>
+      <div>{user.company.catchPhrase}</div>
+      <div>
+        <b>{user.company.bs}</b>
+      </div>
+      <br />
+      <button>Details</button>
+    </div>
+  );
 };
 
 export default UserCard;
